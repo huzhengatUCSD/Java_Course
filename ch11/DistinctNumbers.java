53b9531a1d6102a3438040ff7ac805fd7fdb1a64
huzheng <huzheng.red@gmail.com>
2016-11-08 08:29:44 +0800

    chapter 11

---
+++ ch11/DistinctNumbers.java
@@ -0,0 +1,25 @@
+package ch11;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    
+    Scanner input = new Scanner(System.in);   
+    System.out.print("Enter integers (input ends with 0): ");
+    int value;
+    
+    do {
+      value = input.nextInt(); // Read a value from the input
+      
+      if (!list.contains(value) && value != 0) 
+        list.add(value); // Add the value if it is not in the list
+    } while (value != 0);
+
+    // Display the distinct numbers
+    for (int i = 0; i < list.size(); i++) 
+      System.out.print(list.get(i) + " ");
+  }
+}
