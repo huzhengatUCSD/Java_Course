8d38485d43290c4cd072162f448ecc33307427af
huzheng <huzheng.red@gmail.com>
2017-09-24 02:48:03 +0800

    another src example

---
+++ ch01/WelcomeWithThreeMessages.java
@@ -0,0 +1,9 @@
+package ch01;
+
+public class WelcomeWithThreeMessages { 
+  public static void main(String[] args) { 
+    System.out.println("Programming is fun!");
+    System.out.println("Fundamentals First");
+    System.out.println("Problem Driven");
+  }
+}
