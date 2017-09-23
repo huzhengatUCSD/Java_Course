03a782e9052e157fb3477f28794c1509765bd9e9
huzheng <huzheng.red@gmail.com>
2017-09-24 02:46:43 +0800

    upload a ComplexHelloApp using External JARs

---
+++ ch01/ComplexHelloApp.java
@@ -0,0 +1,18 @@
+package ch01;
+
+import edu.duke.FileResource;
+
+public class ComplexHelloApp{
+	public void runHello () {
+		FileResource res = new FileResource(".//res//ch01//hello_unicode.txt");
+		for (String line : res.lines()) {
+			System.out.print(line+"\n");
+		}
+		
+	}
+    public static void main(String[] args) {
+	   ComplexHelloApp aHello = new 	ComplexHelloApp();
+	   aHello.runHello();
+		
+	}
+}
