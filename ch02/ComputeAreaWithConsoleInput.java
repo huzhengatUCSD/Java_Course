faa87e45b3e66a1edd08ceb902b2f5dc1c3136b8
huzheng <huzheng.red@gmail.com>
2017-10-08 21:50:22 +0800

    ch02 updated in all

--- ch02/ComputeAreaWithConsoleInput.java
+++ ch02/ComputeAreaWithConsoleInput.java
@@ -5,7 +5,7 @@ import java.util.Scanner; // Scanner is in the java.util package
 public class ComputeAreaWithConsoleInput {
   public static void main(String[] args) {
     // Create a Scanner object
-    Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in );
     
     // Prompt the user to enter a radius
     System.out.print("Enter a number for radius: ");
