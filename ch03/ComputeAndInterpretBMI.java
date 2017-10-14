95929a0d53b3291d6da7f4e063355dbe8c0e5c68
huzheng <huzheng.red@gmail.com>
2017-10-14 11:37:05 +0800

    update bmi java for chinese localization

--- ch03/ComputeAndInterpretBMI.java
+++ ch03/ComputeAndInterpretBMI.java
@@ -7,19 +7,14 @@ public class ComputeAndInterpretBMI {
     Scanner input = new Scanner(System.in);
     
     // Prompt the user to enter weight in pounds
-    System.out.print("Enter weight in pounds: ");
-    double weight = input.nextDouble();
+    System.out.print("Enter weight in kilograms: ");
+    double weightInKilograms = input.nextDouble();
     
     // Prompt the user to enter height in inches
-    System.out.print("Enter height in inches: ");
-    double height = input.nextDouble();
-    
-    final double KILOGRAMS_PER_POUND = 0.45359237; // Constant
-    final double METERS_PER_INCH = 0.0254; // Constant 
+    System.out.print("Enter height in meters: ");
+    double heightInMeters = input.nextDouble();
     
     // Compute BMI
-    double weightInKilograms = weight * KILOGRAMS_PER_POUND; 
-    double heightInMeters = height * METERS_PER_INCH; 
     double bmi = weightInKilograms / 
       (heightInMeters * heightInMeters);
 
