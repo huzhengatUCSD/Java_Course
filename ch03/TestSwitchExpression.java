59e4f60acc16593e9e40b51c5690f6a94b54f99c
huzheng <huzheng.red@gmail.com>
2017-10-14 20:27:48 +0800

    TestSwitchExpression.java was not correct, and now has been modified.

--- ch03/TestSwitchExpression.java
+++ ch03/TestSwitchExpression.java
@@ -3,9 +3,18 @@ package ch03;
 public class TestSwitchExpression {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int var=3;
-        System.out.println(--var);
-	}
+		int i=40;
+        switch (i) {
+        case 40:
+            System.out.println("40 was printed");
+            break;
+        case 45:
+            System.out.println("45 was printed");//将会打印这句
+            break;
+        default:
+            System.out.println("? ? ?");
+            break;
+        }
+    }
 
 }
