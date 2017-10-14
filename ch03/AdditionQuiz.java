37d7786c21b83dc64822e33da9c8205c323852ae
huzheng <huzheng.red@gmail.com>
2017-10-14 11:30:57 +0800

    for homework example

--- ch03/AdditionQuiz.java
+++ ch03/AdditionQuiz.java
@@ -3,21 +3,21 @@ package ch03;
 import java.util.Scanner;
 
 public class AdditionQuiz {
-  public static void main(String[] args) {
-    int number1 = (int)(System.currentTimeMillis() % 10);
-    int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+	public static void main(String[] args) {
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
 
-    // Create a Scanner
-    Scanner input = new Scanner(System.in);
+		// Create a Scanner
+		Scanner input = new Scanner(System.in);
 
-    System.out.print(
-      "What is " + number1 + " + " + number2 + "? ");
+		System.out.print(
+				"What is " + number1 + " + " + number2 + "? ");
 
-    int answer = input.nextInt();
+		int answer = input.nextInt();
 
-    System.out.println(
-      number1 + " + " + number2 + " = " + answer + " is " +
-      (number1 + number2 == answer));
-    input.close();
-  }
+		System.out.println(
+				number1 + " + " + number2 + " = " + answer + " is " +
+						(number1 + number2 == answer));
+		input.close();
+	}
 }
