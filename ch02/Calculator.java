093122f74a0ca1d65be38db04d2deee49a36531a
huzheng <huzheng.red@gmail.com>
2018-10-05 12:17:58 +0800

    update ch02 Calculator.java

--- ch02/Calculator.java
+++ ch02/Calculator.java
@@ -11,10 +11,13 @@ public class Calculator {
     public void multiply(int n) {
     }         // 此方法尚未写好，请补充
     public void divide(int n) {
-        result = result / n;
+    	          // 此方法尚未写好，请补充
     }
-    public void square(int n) {
-        result = n * n;
+    public void square() {
+    	         // 此方法尚未写好，请补充
+    }
+    public void power(int n) {
+             // 此方法尚未写好，请补充
     }
     public void clear() {     // 将结果清零
         result = 0;
