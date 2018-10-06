6852d2ddaabab0d01878c9c05558f48c14286777
huzheng <huzheng.red@gmail.com>
2018-10-07 04:30:34 +0800

    update Welcome.java

--- ch01/Welcome.java
+++ ch01/Welcome.java
@@ -5,6 +5,6 @@ package ch01;
 public class Welcome { 
   public static void main(String[] args) { 
     // Display message Welcome to Java! on the console
-    System.out.println("Welcome to Java!");
+    System.out.println("Welcome to Java world!");
   }
 }
