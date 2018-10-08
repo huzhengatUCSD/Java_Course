a2e753d8a23a559cf411d0d8df11a42942d97b6e
huzhengatUCSD <huzheng.red@gmail.com>
2018-10-08 11:08:46 +0800

    update welcomewiththreemessage

--- ch01/WelcomeWithThreeMessages.java
+++ ch01/WelcomeWithThreeMessages.java
@@ -4,6 +4,6 @@ public class WelcomeWithThreeMessages {
   public static void main(String[] args) { 
     System.out.println("Programming is fun!");
     System.out.println("Fundamentals First");
-    System.out.println("Problem Driven");
+    System.out.println("Problem Driven!");
   }
 }
