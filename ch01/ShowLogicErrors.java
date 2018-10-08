48190c40ece54b899c6856f3a547f1c454916e55
huzhengatUCSD <huzheng.red@gmail.com>
2018-10-08 13:08:55 +0800

    update showlogicerros.java

--- ch01/ShowLogicErrors.java
+++ ch01/ShowLogicErrors.java
@@ -3,6 +3,6 @@ package ch01;
 public class ShowLogicErrors {
   public static void main(String[] args) {
     System.out.println("Celsius 35 is Fahrenheit degree ");
-    System.out.println((9 / 5) * 35 + 32);
+    System.out.println((5 / 9) * 35 + 32);
   }
 }
