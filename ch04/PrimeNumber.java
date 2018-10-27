17860cf4bdae97d24ee2a4406423f4935d9eb877
huzhengatUCSD <huzheng.red@gmail.com>
2018-10-27 14:08:27 +0800

    uncompleted PrimeNumber.java

---
+++ ch04/PrimeNumber.java
@@ -0,0 +1,27 @@
+package ch04;
+
+public class PrimeNumber {
+  public static void main(String[] args) {
+    final int NUMBER_OF_PRIMES = 50; // Number of primes to display
+    final int NUMBER_OF_PRIMES_PER_LINE = 10; // Display 10 per line
+    int count = 0; // Count the number of prime numbers
+    int number = 2; // A number to be tested for primeness
+
+    System.out.println("The first 50 prime numbers are \n");
+
+    // Repeatedly find prime numbers
+    while (count < NUMBER_OF_PRIMES) {
+      // Assume the number is prime
+      
+
+      // Test if number is prime
+    
+
+      // Print the prime number and increase the count
+      
+
+      // Check if the next number is prime
+      number++;
+    }
+  }
+}
