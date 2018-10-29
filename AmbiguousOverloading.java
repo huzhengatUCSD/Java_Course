7d1bcbf21465f38591df3e1404f2be8ccd4478e3
huzhengatUCSD <huzheng.red@gmail.com>
2018-10-29 13:17:18 +0800

    AmbiguousOverloading update

---
+++ AmbiguousOverloading.java
@@ -0,0 +1,20 @@
+public class AmbiguousOverloading {
+  public static void main(String[] args) {
+    System.out.println(max(1, 2));
+    
+  }
+  
+  public static double max(int num1, double num2) { 
+    if (num1 > num2)
+      return num1;
+    else
+      return num2;
+  }
+  
+  public static double max(double num1, int num2) {
+    if (num1 > num2)
+      return num1;
+    else
+      return num2;     
+  }
+}
