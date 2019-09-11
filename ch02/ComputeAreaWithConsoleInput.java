ab14f4e933692f3d022f914242ebf73e47f10aac
huzheng <huzheng.red@gmail.com>
2019-09-11 19:24:46 +0800

    try with resource usage update

--- ch02/ComputeAreaWithConsoleInput.java
+++ ch02/ComputeAreaWithConsoleInput.java
@@ -5,19 +5,19 @@ import java.util.Scanner; // Scanner is in the java.util package
 public class ComputeAreaWithConsoleInput {
   public static void main(String[] args) {
     // Create a Scanner object
-    Scanner input = new Scanner(System.in );
-    
-    // Prompt the user to enter a radius
-    System.out.print("Enter a number for radius: ");
-    double radius = input.nextDouble();
+	  Scanner input = new Scanner(System.in );
 
-    // Compute area
-    double area = radius * radius * 3.14159;
+	  // Prompt the user to enter a radius
+	  System.out.print("Enter a number for radius: ");
+	  double radius = input.nextDouble();
 
-    // Display result
-    System.out.println("The area for the circle of radius " +
-      radius + " is " + area);
-    
-    input.close();
+	  // Compute area
+	  double area = radius * radius * 3.14159;
+
+	  // Display result
+	  System.out.println("The area for the circle of radius " +
+			  radius + " is " + area);
+
+	  input.close();
   } 
 }
--- ch02/ComputeAverage.java
+++ ch02/ComputeAverage.java
@@ -5,21 +5,21 @@ import java.util.Scanner; // Scanner is in the java.util package
 public class ComputeAverage {
   public static void main(String[] args) {
     // Create a Scanner object
-    Scanner in = new Scanner(System.in);
+	  try(Scanner in = new Scanner(System.in)){
 
-    
-    // Prompt the user to enter three numbers
-    System.out.print("Enter three numbers: ");
-    double number1 = in.nextDouble();
-    double number2 = in.nextDouble();
-    double number3 = in.nextDouble();
 
-    // Compute average
-    double average = (number1 + number2 + number3) / 3;
+		  // Prompt the user to enter three numbers
+		  System.out.print("Enter three numbers: ");
+		  double number1 = in.nextDouble();
+		  double number2 = in.nextDouble();
+		  double number3 = in.nextDouble();
 
-    // Display result
-    System.out.println("The average of " + number1 + " " + number2
-      + " " + number3 + " is " + average);
-    in.close();
+		  // Compute average
+		  double average = (number1 + number2 + number3) / 3;
+
+		  // Display result
+		  System.out.println("The average of " + number1 + " " + number2
+				  + " " + number3 + " is " + average);
+	  }
   } 
 }
