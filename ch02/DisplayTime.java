84ab62a784e6ee734cddb89a578d0d41c6bcf3e8
huzheng <huzheng.red@gmail.com>
2020-09-14 20:53:25 +0800

    ch01 ch02 updated with src and slides as well as Exercise_01

--- ch02/DisplayTime.java
+++ ch02/DisplayTime.java
@@ -8,10 +8,12 @@ public class DisplayTime {
     // Prompt the user for input
     System.out.print("Enter an integer for seconds: ");
     int seconds = input.nextInt();
- 
-    int minutes = seconds / 60; // Find minutes in seconds
+    
+    int hours = seconds / 3600; // Find hours in seconds
     int remainingSeconds = seconds % 60; // Seconds remaining
-    System.out.println(seconds + " seconds is " + minutes + 
+    int minutes = remainingSeconds / 60; // Find minutes in seconds
+    remainingSeconds = remainingSeconds % 60; // Seconds remaining
+    System.out.println(seconds + " seconds is " + hours + " hours and "  + minutes + 
       " minutes and " + remainingSeconds + " seconds");  
     input.close();
   }
