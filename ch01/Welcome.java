f71749c27adfecee910cb3f6511e50240bb1248b
huzheng <huzheng.red@gmail.com>
2021-09-15 12:54:21 +0800

    git commit test

--- ch01/Welcome.java
+++ ch01/Welcome.java
@@ -5,7 +5,7 @@ package ch01;
 public class Welcome { 
   public static void main(String[] args) { 
     // Display message Welcome to Java! on the console
-    System.out.println("Welcome to Java@2021Autumn BUPT!"); //to do
+    System.out.println("Welcome to Java @2021Autumn BUPT!"); //to do
   }
 }
 
