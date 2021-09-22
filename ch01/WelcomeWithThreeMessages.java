1ceadfb727e7d5d95f2980722625628dfc7c73e4
huzhengatUCSD <huzheng.red@gmail.com>
2021-09-23 06:06:55 +0800

    two welcome srcs updated

--- ch01/WelcomeWithThreeMessages.java
+++ ch01/WelcomeWithThreeMessages.java
@@ -5,5 +5,7 @@ public class WelcomeWithThreeMessages {
     System.out.println("Programming is fun!");
     System.out.println("Fundamentals First");
     System.out.println("Problem Driven!");
+    var var="有点意思";
+    System.out.println(var);
   }
 }
