2c605d2cc20bebedc71a167b43d9ab7f411950c0
huzhengatUCSD <huzheng.red@gmail.com>
2022-09-07 23:43:16 +0800

    test updating Welcome.java

--- ch01/Welcome.java
+++ ch01/Welcome.java
@@ -3,9 +3,14 @@
 package ch01;
 
 public class Welcome { 
+	public void hello() {
+		System.out.println("Welcome to Java!"); 
+	}
   public static void main(String[] args) { 
     // Display message Welcome to Java! on the console
     System.out.println("Welcome to Java!"); //to do
+    new Welcome().hello();
+    
   }
 }
 
