bcb544457ff5ae7a967332d4da1f19d581aa4186
huzhengatUCSD <huzheng.red@gmail.com>
2022-09-14 22:12:01 +0800

    revise syntaxerros in ch01-just a test

--- ch01/ShowSyntaxErrors.java
+++ ch01/ShowSyntaxErrors.java
@@ -1,7 +1,7 @@
 package ch01;
 
 public class ShowSyntaxErrors {
-  public static main(String[] args) {
-    System.out.println("Welcome to Java);
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java");
   }
 }
