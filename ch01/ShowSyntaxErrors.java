dd56104c7c70aec43e88c22e5ce65fe7bf47fdc9
huzhengatUCSD <huzheng.red@gmail.com>
2022-09-14 22:26:04 +0800

    ShowSyntaxErrors.java updated for test

--- ch01/ShowSyntaxErrors.java
+++ ch01/ShowSyntaxErrors.java
@@ -2,6 +2,6 @@ package ch01;
 
 public class ShowSyntaxErrors {
   public static void main(String[] args) {
-    System.out.println("Welcome to Java");
+    System.out.println(" Welcome to Java ");
   }
 }
