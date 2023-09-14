61ac9e9a226e93a4ac4fc87513af78dfa3984c09
huzhengatBUPT <huzheng.red@gmail.com>
2023-09-14 23:14:05 +0800

    2023年课程提交Welcome.java更新示例

--- ch01/Welcome.java
+++ ch01/Welcome.java
@@ -4,11 +4,11 @@ package ch01;
 
 public class Welcome { 
 	public void hello() {
-		System.out.println("Hello, Welcome to Java! "); 
+		System.out.println("Hello, Welcome to Java World! "); 
 	}
   public static void main(String[] args) { 
     // Display message Welcome to Java! on the console
-    System.out.println("Welcome to Java! "); //to do
+    System.out.println("Welcome to Java world and something new happened! "); //to do
     new Welcome().hello();
     
   }
